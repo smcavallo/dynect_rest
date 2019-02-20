884482b929fd7e1380f7568029eeb9d64ec56b3e
S.Cavallo <smcavallo@hotmail.com>
2019-02-19 20:40:50 -0500

    rubocop fixes only Signed-off-by: S.Cavallo <smcavallo@hotmail.com>

--- lib/dynect_rest/exceptions.rb
+++ lib/dynect_rest/exceptions.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,14 +21,12 @@ class DynectRest
     class RequestFailed < RuntimeError; end
     # we need to handle API calls that return a status of 'incomplete' and return the job_id
     class IncompleteRequest < RuntimeError
+      attr_reader :job_id, :message
 
-        attr_reader :job_id, :message
-
-        def initialize( message, job_id )
-            @message = message
-            @job_id = job_id
-        end
-
+      def initialize(message, job_id)
+        @message = message
+        @job_id = job_id
+      end
     end
   end
 end
--- spec/dynect_rest_spec.rb
+++ spec/dynect_rest_spec.rb
@@ -1,74 +1,73 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe DynectRest do
-
   let(:dynect) do
-    DynectRest.new("customer", "username", "password", "zone", false)
+    DynectRest.new('customer', 'username', 'password', 'zone', false)
   end
 
-  it "A Record" do
+  it 'A Record' do
     expect(dynect.a.resource_path).to eq('ARecord/zone')
   end
 
-  it "AAAA Record" do
+  it 'AAAA Record' do
     expect(dynect.aaaa.resource_path).to eq('AAAARecord/zone')
   end
 
-  it "CNAME Record" do
+  it 'CNAME Record' do
     expect(dynect.cname.resource_path).to eq('CNAMERecord/zone')
   end
 
-  it "DNSKEY Record" do
+  it 'DNSKEY Record' do
     expect(dynect.dnskey.resource_path).to eq('DNSKEYRecord/zone')
   end
 
-  it "DS Record" do
+  it 'DS Record' do
     expect(dynect.ds.resource_path).to eq('DSRecord/zone')
   end
 
-  describe "gslb" do
+  describe 'gslb' do
     subject { dynect.gslb }
     its(:resource_path) { should == 'GSLB/zone' }
   end
 
-  describe "key" do
+  describe 'key' do
     subject { dynect.key }
     its(:resource_path) { should == 'KEYRecord/zone' }
   end
 
-  it "KEY Record" do
+  it 'KEY Record' do
     expect(dynect.key.resource_path).to eq('KEYRecord/zone')
   end
 
-  it "LOC Record" do
+  it 'LOC Record' do
     expect(dynect.loc.resource_path).to eq('LOCRecord/zone')
   end
 
-  it "MX Record" do
+  it 'MX Record' do
     expect(dynect.mx.resource_path).to eq('MXRecord/zone')
   end
 
-  it "NS Record" do
+  it 'NS Record' do
     expect(dynect.ns.resource_path).to eq('NSRecord/zone')
   end
 
-  it "PTR Record" do
+  it 'PTR Record' do
     expect(dynect.ptr.resource_path).to eq('PTRRecord/zone')
   end
 
-  it "RPR Record" do
+  it 'RPR Record' do
     expect(dynect.rp.resource_path).to eq('RPRecord/zone')
   end
 
-  it "SOA Record" do
+  it 'SOA Record' do
     expect(dynect.soa.resource_path).to eq('SOARecord/zone')
   end
 
-  it "SRV Record" do
+  it 'SRV Record' do
     expect(dynect.srv.resource_path).to eq('SRVRecord/zone')
   end
 
-  it "TXT Record" do
+  it 'TXT Record' do
     expect(dynect.txt.resource_path).to eq('TXTRecord/zone')
   end
 end
